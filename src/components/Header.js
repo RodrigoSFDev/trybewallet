5667f2135bf6be2f8c0fe1701f2313987e36a6b8
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-05-12 19:43:54 -0300

    requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+  state = {
+    despesa: 0,
+    cambio: 'BRL',
+  };
+
   render() {
+    const { despesa, cambio } = this.state;
     const { email } = this.props;
     return (
-      <div>{email}</div>
+      <div>
+        <div data-testid="email-field">{email}</div>
+        <div data-testid="total-field">{despesa}</div>
+        <div data-testid="header-currency-field">{cambio}</div>
+      </div>
     );
   }
 }
