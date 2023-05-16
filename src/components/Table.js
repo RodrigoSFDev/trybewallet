14e8e6cf7f8f5ac349663a3a6c98e10450a2e7a5
Rodrigo Silva Ferreira <rodrigosferreira0@gmail.com>
2023-05-15 22:15:28 -0300

    requisito 6 e 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir.</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              expenses.map((expense, index) => (
+                <tr key={ index }>
+                  <td>{expense.description}</td>
+                  <td>{expense.tag}</td>
+                  <td>{expense.method}</td>
+                  <td>{Number(expense.value).toFixed(2)}</td>
+                  <td>{expense.exchangeRates[expense.currency].name}</td>
+                  <td>
+                    {Number(expense.exchangeRates[expense.currency].ask)
+                      .toFixed(2)}
+
+                  </td>
+                  <td>
+                    {(Number(expense.value)
+                  * Number(expense.exchangeRates[expense.currency].ask))
+                      .toFixed(2)}
+
+                  </td>
+                  <td>Real</td>
+                  <td><button type="button">Editar</button></td>
+
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+
+      </div>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+export default connect(mapStateToProps)(Table);
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
